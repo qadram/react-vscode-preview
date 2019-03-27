2df1139b30e33beba6b2de89d726f6f50658b2b8
qadram <qadram@gmail.com>
2019-03-27 19:09:58 +0100

    Add files via upload

---
+++ scripts/utils/verifyPackageTree.js
@@ -0,0 +1,12 @@
+// @remove-file-on-eject
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+'use strict';
+
+const verifyPackageTree = require('react-scripts/scripts/utils/verifyPackageTree');
+module.exports = verifyPackageTree
